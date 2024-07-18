58bdae677bb41388fd69446e4b003980e28a3509
Pessé Essile <151667097+PesseEssille@users.noreply.github.com>
2024-07-18 21:10:42 +0200

    Update DemmaRetard.cmd
    Instructions en variables
    commentaires en ligne avec & non précédés d'un espace

--- DemmaRetard.cmd
+++ DemmaRetard.cmd
@@ -13,8 +13,11 @@ CALL :log %clnfl% purge fichiers temporaires
 CALL :log suppression des fichiers temporaires de plus de 7j
 %cscpt% %clnfl% -L /S>> %FicLog%
 
-CALL :log suppression des journaux de d‚marrage de plus de 7j
+CALL :log suppression des journaux de démarrage de plus de 7j
 %cscpt% %clnfl% -r:"%RepLog%" -L>> %FicLog%
+
+CALL :log suppression des fichiers d'Outlook de plus de 15j
+%cscpt% %clnfl% -r:"C:\Users\pcsi-\AppData\Local\Packages\microsoft.windowscommunicationsapps_8wekyb3d8bbwe\LocalState\Files\S0" -L -d:15>> %FicLog%
 
 SET cscpt=
 SET clnfl=
@@ -65,38 +68,40 @@ GOTO :eof
 
 REM pause d'un nombre de secondes
 :sleep
-SET /a nbsec=%1+1
-CALL :log %1 secondes d'attente. %2 %3 %4 %5 %6 %7 %8 %9
-ping -n %nbsec% localhost > nul
+SET /a NbSec=%1+1
+SET AllPrm=%*
+FOR /F "tokens=1*" %%a in ("%AllPrm%") DO SET Msg=%%b
+CALL :log %1 secondes d'attente. %Msg%
+ping -n %NbSec% localhost > nul
 GOTO :eof
 
 (
-Attente qu'un ex‚cutable soit en mode running
-PARAM 1 : nom de l'ex‚cutable. ajout de .exe si absent … la fin
-paramŠtres suivants : nom de la fenˆtre, si besoin
+Attente qu'un exécutable soit en mode running
+PARAM 1 : nom de l'exécutable. ajout de .exe si absent à la fin
+paramètres suivants : nom de la fenêtre, si besoin
 ) 2>nul
 :attend_lance
-SET FicExe=%1   &:: Le 1er paramŠtre contient le nom de l'ex‚cutable
+SET FicExe=%1&:: Le 1er paramètre contient le nom de l'exécutable
 IF NOT "%FicExe:~-4%" == ".exe" SET FicExe=%FicExe%.exe
-REM les paramŠtres suivants, facultatifs, contiennent le nom de la fenˆtre
+REM les paramètres suivants, facultatifs, contiennent le nom de la fenêtre
 SET AllPrm=%*
 FOR /F "tokens=1*" %%a in ("%AllPrm%") DO SET NomFen=%%b
 
-SET essai=1 &:: compteur du nombre d'essais
-SET CodRet=KO &:: code de retour de la fonction
+SET essai=1&REM compteur du nombre d'essais
+SET CodRet=KO&REM code de retour de la fonction
 
-REM tƒche avec le nom de l'application et du statut
-SET cmd2run=tasklist /nh /v /fi ^"IMAGENAME eq %FicExe%^" /fi ^"STATUS eq RUNNING^" &:: commande … ex‚cuter
-REM si le nom de la fenˆtre est fournis, on le concatŠne avec la commande
+REM tâche avec le nom de l'application et du statut
+SET cmd2run=tasklist /nh /v /fi ^"IMAGENAME eq %FicExe%^" /fi ^"STATUS eq RUNNING^"&REM commande à exécuter
+REM si le nom de la fenêtre est fournis, on le concatène avec la commande
 IF "%NomFen%" NEQ "" SET cmd2run=%cmd2run% /fi ^"WINDOWTITLE eq %NomFen%^"
 REM filtre du retour de la commande pour avoir un code d'erreur
 SET cmd2fnd=%cmd2run% ^| findstr /i /r ^"^^%FicExe% ^"
 
 :boucle
-ECHO ______________ essai num‚ro {%essai%} ______________
-ECHO ______________ essai num‚ro {%essai%} ______________>>%FicLog%
+ECHO ______________ essai numéro {%essai%} ______________
+ECHO ______________ essai numéro {%essai%} ______________>>%FicLog%
 
-REM liste des tƒches avec le nom de l'application, le statut et en option le nom de la fenˆtre
+REM liste des tâches avec le nom de l'application, le statut et en option le nom de la fenêtre
 REM sortie terminal
 SET cmd2run
 %cmd2run%
@@ -104,7 +109,7 @@ REM sortie journal
 SET cmd2run>>%FicLog%
 %cmd2run%>>%FicLog% 2>&1
 
-REM filtr‚ sur le nom, pour avoir un code erreur
+REM filtré sur le nom, pour avoir un code erreur
 REM sortie terminal
 SET cmd2fnd
 %cmd2fnd%
@@ -114,7 +119,6 @@ SET cmd2fnd>>%FicLog%
 
 IF ERRORLEVEL 1 (SET CodRet=KO) ELSE (SET CodRet=OK)
 CALL :log CodRet=[%CodRet%]
-CALL :log CodRet=[%CodRet%]>>%FicLog%
 IF "%CodRet%" == "OK" GOTO :eof
 
 SET /A essai=essai+1
